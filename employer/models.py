7a803fd67d216c3469669316894c7ecf2726c8a5
parkjaeseong <wotjd0715@gmail.com>
2020-06-10 18:10:49 +0900

    0610jsemployer

--- employer/models.py
+++ employer/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from accounts.models import User
 
+# Create your models here.
+class Article(models.Model):
+    postid      = models.CharField(max_length=20)
+    menuid      = models.CharField(max_length=120,default='')
+    title       = models.CharField(max_length=120)
+    text        = models.TextField()
+    place       = models.CharField(max_length=120)
+    price       = models.CharField(max_length=120,default = '')
+    created_at  = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.menuid
 # Create your models here.
--- employer/urls.py
+++ employer/urls.py
@@ -5,5 +5,16 @@ from .views import *
 app_name = 'employer'
 
 urlpatterns = [
-    path('', employer_all, name='employer_all'),
+    path('',employer_all, name='employer_all'),
+    path('new/',new_feed, name='new_feed'),
+    path('feed/<pk>/', detail_feed),
+    path('feed/<pk>/remove/', remove_feed),
+    path('feed/<pk>/edit/', edit_feed),
+    path('home/', sort1, name='home'),
+    path('load/',sort2, name='load'),
+    path('pet/',sort3, name='pet'),
+    path('acting/',sort4, name='acting'),
+    path('short/',sort5, name='short'),
+    path('carpool/',sort6, name='carpool'),
+    path('etc/',sort7, name='etc'),
 ]
