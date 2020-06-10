f2141b585648f88227361bfb4015afd3d7e18be8
LJBang <kailo123457@gmail.com>
2020-06-10 16:21:52 +0900

    comments

---
+++ staticfiles/js/inputchecker.js
@@ -0,0 +1,22 @@
+function inputChecker(){
+        if(inputForm.menuid.value == "게시판선택"){
+            alert("카테고리를 골라주세요");
+            return false;
+        }
+        if(inputForm.title.value.length<1){
+            alert("제목을 입력하세요");
+            return false;
+        }
+        if(inputForm.place.value.length<1){
+            alert("장소를 입력하세요");
+            return false;
+        }
+        if(inputForm.price.value.length<1){
+            alert("금액을 입력하세요");
+            return false;
+        }
+        if(inputForm.text.value.length<1){
+            alert("내용을 입력하세요");
+            return false;
+        }
+    }
